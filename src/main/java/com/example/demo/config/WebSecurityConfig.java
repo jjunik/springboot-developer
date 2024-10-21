4004c302293fcc17635e3fb0285d9a4fd794a23b
jjunik <tjwnsdlr4545@gmail.com>
2024-10-21 11:33:21 +0900

    dns추가

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig {
       CorsConfiguration configuration = new CorsConfiguration();
       // React 애플리케ㅐ이션이 실행되는 출처에서 오는 요청을 허용
       configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
-    		  "http://react-developer-env.eba-quxumgny.ap-northeast-2.elasticbeanstalk.com")); // 프론트엔드 주소
+    		  "http://app.todojik.shop")); // 프론트엔드 주소
       // HTTP 메서드 허용
       configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
       // 모든 헤더를 허용
